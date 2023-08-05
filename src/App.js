e4208024eed45d357d891237e0f0afbdc8e47b58
jirishus <jirishus@yahoo.com>
2023-08-05 10:16:00 -0700

    basic barchart with test data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import { UserData } from "./Data";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>MooCharts</h3>
-        <p>
-          Let's get Mooving
-        </p>
-      </header>
+      <div style={{ width: 700 }}>
+        <BarChart chartData={userData} />
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,20 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2016,
+        userGain: 8000,
+        userLost: 500
+    },
+    {
+        id: 2,
+        year: 2016,
+        userGain: 8000,
+        userLost: 500
+    },
+    {
+        id: 3,
+        year: 2016,
+        userGain: 18000,
+        userLost: 50
+    }
+]
